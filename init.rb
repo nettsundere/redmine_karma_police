e064743b45200875faff3f412d48e846828052eb
Vladimir Kiselev <nettsundere@gmail.com>
2011-11-27 06:48:53 +0400

    Init file changed.

--- init.rb
+++ init.rb
@@ -3,8 +3,9 @@ require 'redmine'
 Redmine::Plugin.register :redmine_karma_police do
   name 'Redmine Karma Police plugin'
   author 'Vladimir `nettsundere` Kiselev'
-  description 'This is a karma-enabled plugin for Redmine. Now every user have a karma!'
+  description 'This is a karma-enabled plugin for Redmine. Now every user has a karma!'
   version '0.0.1'
   url 'https://github.com/nettsundere/redmine_karma_police'
   author_url 'http://rue-m.ru'
+  menu :application_menu, :redmine_karma_police, { :controller => 'karma', :action => 'index' }, :caption => 'Karma'
 end
